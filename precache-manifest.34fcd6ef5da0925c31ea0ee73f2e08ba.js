19d30e5038d72bffd9788ef06f1627e55d4633b8
Itamar Tati <itamar.tati1994@gmail.com>
2020-03-08 21:44:32 +0000

    Updates

---
+++ precache-manifest.34fcd6ef5da0925c31ea0ee73f2e08ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50fc6be85aa323cfa46fee6048b8a9fb",
+    "url": "/flickr-api/index.html"
+  },
+  {
+    "revision": "5604d49502ac10cec053",
+    "url": "/flickr-api/static/css/main.da6b6583.chunk.css"
+  },
+  {
+    "revision": "0865216b2f525f78a66f",
+    "url": "/flickr-api/static/js/2.feef0131.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/flickr-api/static/js/2.feef0131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5604d49502ac10cec053",
+    "url": "/flickr-api/static/js/main.a711c99b.chunk.js"
+  },
+  {
+    "revision": "0a9c9ce847af98224297",
+    "url": "/flickr-api/static/js/runtime-main.dc94298a.js"
+  }
+]);
